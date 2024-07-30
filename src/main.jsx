13cce726ec1b7e4885500f57fb6a4155096b54fd
swapnesh <swapnesha839@gmail.com>
2024-07-30 15:03:37 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/onepager">
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='*' element={<App />} />
-      </Routes>
-    </BrowserRouter>,
+    <App/>
   </React.StrictMode>,
 )
