74648b5da6c90ea3212aec9877400bbcc59b462f
swapnesh <swapnesha839@gmail.com>
2024-07-30 16:16:01 +0530

    no path

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,13 @@ import logo from "../src/assets/images/starlllylogo.png"
 import bulb from "../src/assets/images/bulb.svg"
 import network from "../src/assets/images/network.svg"
 import { Card, Col, Container, Image, Nav, Navbar, Row } from 'react-bootstrap'
-import { Facebook, Linkedin, Twitter } from 'lucide-react'
+import { Instagram, Linkedin } from 'lucide-react'
 
 function App() {
 
-  const { ref: myRef2, inView: inView2 } = useInView({ triggerOnce: true });
-  const { ref: myRef3, inView: inView3 } = useInView({ triggerOnce: true });
-  const { ref: myRef4, inView: inView4 } = useInView({ triggerOnce: true });
+  const { ref: myRef2, inView: inView2 } = useInView({ triggerOnce: false });
+  const { ref: myRef3, inView: inView3 } = useInView({ triggerOnce: false });
+  const { ref: myRef4, inView: inView4 } = useInView({ triggerOnce: false });
   const { ref: myRef1, inView: inView1 } = useInView({ triggerOnce: false });
   useEffect(() => {
     AOS.init({
@@ -65,6 +65,7 @@ function App() {
   )
 }
 const Section1 = ({ inView1, myRef1 = true }) => {
+  const [videoError, setVideoError] = useState(false);
 
   return (
     <Row>
@@ -78,7 +79,7 @@ const Section1 = ({ inView1, myRef1 = true }) => {
             {/* Your partner in business excellence */}
             Build a customised approach in handling analytics/workflow automation/data representation essential for your business, by availing our services
           </p>
-          <a target="_blank" href='https://starllysolutions-my.sharepoint.com/:p:/g/personal/gkadkol_starlly_in/Efa5tFw7yShdtbRRqNCyIfYBeu2RmlFjHhlZvO-y_9U0Ug?e=N4NHGF' className='section1btn rounded-5 text-white text-decoration-underline'>LEARN MORE</a>
+          <a href='/' className='section1btn rounded-5 text-white text-decoration-underline'>LEARN MORE</a>
         </div>
       </Col>
       <Col md="6"
@@ -86,7 +87,7 @@ const Section1 = ({ inView1, myRef1 = true }) => {
       // className={`p-0 m-0 h-100 ${inView1 ? 'animate__animated' : ''}`}
       >
         <Container ref={myRef1} style={{ backgroundColor: "#1AC784" }} data-aos="fade-up">
-          <video
+          {/* <video
             autoPlay
             loop
             // controls
@@ -96,13 +97,30 @@ const Section1 = ({ inView1, myRef1 = true }) => {
           >
             <source src={video1} type="video/mp4" />
             <Image src={image1} width={"100%"} />
-          </video>
+          </video> */}
+          {!videoError ? (
+            <video
+              autoPlay
+              loop
+              muted
+              width="100%"
+              className='p-0'
+              style={{ backgroundColor: "#1AC784", width: '100%' }}
+              onError={() => setVideoError(true)}
+            >
+              <source src={video1} type="video/mp4" />
+              <p>Your browser does not support the video tag.</p>
+            </video>
+          ) : (
+            <img src={image1} alt="Fallback" style={{ width: '100%' }} />
+          )}
         </Container>
       </Col>
     </Row>
   )
 }
 const Section2 = ({ myRef2, inView2 }) => {
+  const [videoError, setVideoError] = useState(false);
   return (
     <Row>
       <Col md="12" className='p-5 text-center d-flex flex-column'>
@@ -116,7 +134,7 @@ const Section2 = ({ myRef2, inView2 }) => {
 
       <Col md="12" className='p-5 m-0 h-100 d-flex'>
         <div ref={myRef2} className='m-auto' data-aos="fade-up" style={{ width: "500px" }}>
-          <video
+          {/* <video
             autoPlay
             loop
             // controls
@@ -126,7 +144,23 @@ const Section2 = ({ myRef2, inView2 }) => {
           >
             <source src={video2} type="video/mp4" />
             <Image src={image2} width={"100%"} />
-          </video>
+          </video> */}
+          {!videoError ? (
+            <video
+              autoPlay
+              loop
+              muted
+              width="100%"
+              className='p-0'
+              style={{ backgroundColor: "#1AC784", width: '100%' }}
+              onError={() => setVideoError(true)}
+            >
+              <source src={video2} type="video/mp4" />
+              <p>Your browser does not support the video tag.</p>
+            </video>
+          ) : (
+            <img src={image2} alt="Fallback" style={{ width: '100%' }} />
+          )}
         </div>
       </Col>
     </Row>
@@ -194,99 +228,118 @@ const Section3 = () => (
     </Col>
   </Row>
 )
-const Section4 = ({ inView3, myRef3 }) => (
-  <Row className='section4'>
-    <Col md="6" className="text-white d-flex p-5 section4">
-      <Container className='section4boxt bg-white w-100 text-black'>
-        <div>
-          <p className='section4heading'>We are good at</p>
-          <ul className="section4text">
-            <li>
-              Dashboards
-            </li>
-            <li>
-              Workflows/Sequence implementation
-            </li>
-            <li>
-              Automation
-            </li>
-            <li>
-              Bot development
-            </li>
-            <li>
-              Analytics and ML/AI
-            </li>
-            <li>
-              Resource Augmentation
-            </li>
-            <li>
-              Content guidance
-            </li>
-            <li>
-              Build-Operate-Transfer model
-            </li>
-            <li>
-              No-Code/Low Code implementation
-            </li>
-            <li>
-              Plugins and Templates
-            </li>
-            <li>
-              IOT-Connected-Smart Device
-            </li>
-          </ul>
-        </div>
-      </Container>
-    </Col>
-    <Col md="6" className='p-0 m-0 h-100'>
-      <Container ref={myRef3} className='p-0' data-aos="fade-up">
-        <video
-          autoPlay
-          loop
-          // controls
-          width="100%"
-          className='p-0'
-          style={{ backgroundColor: "#CDF3E5", width: '100%' }}
-        >
-          <source src={video3} type="video/mp4" />
-          <Image src={image3} width={"100%"} />
-        </video>
-      </Container>
-    </Col>
-    <Row className='pb-5'>
-      <Col md="10" className='section4'>
+const Section4 = ({ inView3, myRef3 }) => {
+  const [videoError, setVideoError] = useState(false);
+  return (
+    <Row className='section4'>
+      <Col md="6" className="text-white d-flex p-5 section4">
         <Container className='section4boxt bg-white w-100 text-black'>
           <div>
-            <p className='section4heading'>Categories we serve</p>
-            <Row>
-              <Col md="4">
-                <ul className="section4text">
-                  <li>Machine/equipment/Robots</li>
-                  <li>DTC & Ecommerce</li>
-                </ul>
-              </Col>
-              <Col md="4">
-                <ul className="section4text">
-                  <li>Wealth Management</li>
-                  <li>Freight/Port/Logistics</li>
-                </ul>
-              </Col>
-              <Col md="4">
-                <ul className="section4text">
-                  <li>SaaS Hubs</li>
-                  <li>Hold Co and SMB</li>
-                </ul>
-              </Col>
-            </Row>
+            <p className='section4heading'>We are good at</p>
+            <ul className="section4text">
+              <li>
+                Dashboards
+              </li>
+              <li>
+                Workflows/Sequence implementation
+              </li>
+              <li>
+                Automation
+              </li>
+              <li>
+                Bot development
+              </li>
+              <li>
+                Analytics and ML/AI
+              </li>
+              <li>
+                Resource Augmentation
+              </li>
+              <li>
+                Content guidance
+              </li>
+              <li>
+                Build-Operate-Transfer model
+              </li>
+              <li>
+                No-Code/Low Code implementation
+              </li>
+              <li>
+                Plugins and Templates
+              </li>
+              <li>
+                IOT-Connected-Smart Device
+              </li>
+            </ul>
           </div>
         </Container>
       </Col>
-      <Col md="2" className='d-flex mt-4'>
-        <a target="_blank" href='https://starllysolutions-my.sharepoint.com/:p:/g/personal/gkadkol_starlly_in/EcO0NXP5dGpWDmPEYmodtG4BnvIW9gxIenTd041ZQt_YjQ?e=SDbApz' className='section4btn py-2 rounded-5 text-white'>CHECK</a>
+      <Col md="6" className='p-0 m-0 h-100'>
+        <Container ref={myRef3} className='p-0' data-aos="fade-up">
+          {/* <video
+              autoPlay
+              loop
+              // controls
+              width="100%"
+              className='p-0'
+              style={{ backgroundColor: "#CDF3E5", width: '100%' }}
+            >
+              <source src={video3} type="video/mp4" />
+              <Image src={image3} width={"100%"} />
+            </video> */}
+          {!videoError ? (
+            <video
+              autoPlay
+              loop
+              muted
+              width="100%"
+              className='p-0'
+              style={{ backgroundColor: "#1AC784", width: '100%' }}
+              onError={() => setVideoError(true)}
+            >
+              <source src={video3} type="video/mp4" />
+              <p>Your browser does not support the video tag.</p>
+            </video>
+          ) : (
+            <img src={image3} alt="Fallback" style={{ width: '100%' }} />
+          )}
+        </Container>
       </Col>
+      <Row className='pb-5'>
+        <Col md="10" className='section4'>
+          <Container className='section4boxt bg-white w-100 text-black'>
+            <div>
+              <p className='section4heading'>Categories we serve</p>
+              <Row>
+                <Col md="4">
+                  <ul className="section4text">
+                    <li>Machine/equipment/Robots</li>
+                    <li>DTC & Ecommerce</li>
+                  </ul>
+                </Col>
+                <Col md="4">
+                  <ul className="section4text">
+                    <li>Wealth Management</li>
+                    <li>Freight/Port/Logistics</li>
+                  </ul>
+                </Col>
+                <Col md="4">
+                  <ul className="section4text">
+                    <li>SaaS Hubs</li>
+                    <li>Hold Co and SMB</li>
+                  </ul>
+                </Col>
+              </Row>
+            </div>
+          </Container>
+        </Col>
+        <Col md="2" className='d-flex mt-4'>
+          <a href='/' className='section4btn py-2 rounded-5 text-white'>CHECK</a>
+        </Col>
+      </Row>
     </Row>
-  </Row>
-)
+  )
+}
 
 const Section5 = () => (
   <div className='py-5'>
@@ -299,7 +352,7 @@ const Section5 = () => (
           <p className='section5heading2'>Spectra</p>
           <p className='section5text'>Simple plug-n-play solution to monitor your machines remotely in real-time, send alerts to the right team members</p>
           <div className="d-flex mt-3">
-            <a target="_blank" href='http://spectra.starllysolutions.com/' className='section5btn rounded-5 py-3 text-white px-4'>LEARN MORE</a>
+            <a href='/' className='section5btn rounded-5 py-3 text-white px-4'>LEARN MORE</a>
           </div>
         </div>
       </Col>
@@ -308,7 +361,7 @@ const Section5 = () => (
           <p className='section5heading2'>Servy</p>
           <p className='section5text'>Servy helps you manage and streamline your service pipeline, helping you track business interactions efficiently</p>
           <div className="d-flex mt-3">
-            <a target="_blank" href='http://servy.starlly.in/' className='section5btn rounded-5 py-3 text-white px-4'>LEARN MORE</a>
+            <a href='/' className='section5btn rounded-5 py-3 text-white px-4'>LEARN MORE</a>
           </div>
         </div>
       </Col>
@@ -338,14 +391,14 @@ const Section6 = ({ inView4, myRef4 }) => (
         <div className='m-md-auto'>
           <p className='section6heading'>Contact Us</p>
           <p className='section6text1'>PHONE</p>
-          <p className='section6text2'>+91 9886506163</p>
+          <p className='section6text2'>+91 9348039447</p>
           <p className='section6text1'>EMAIL</p>
-          <p className='section6text2'>gkadkol@starlly.in</p>
+          <p className='section6text2'>business.swapnesh@gmail.com</p>
           <p className='section6text1'>SOCIAL</p>
           <div>
-            <a target="_blank" href="https://www.facebook.com/people/Starlly-Solutions-Pvt-Ltd/100089368485327/?sk=about"><Facebook className='section6text2' /></a>
-            <a target="_blank" href='https://x.com/sayhitospectra'><Twitter className='section6text2 mx-4' /></a>
-            <a target="_blank" href='https://www.linkedin.com/company/starlly-solutions-pvt.-ltd./'><Linkedin className='section6text2 mx-2' /></a>
+            <a href="https://www.instagram.com/swapnesh.io/"><Instagram className='section6text2' /></a>
+            {/* <a  href='https://x.com/sayhitospectra'><Twitter className='section6text2 mx-4' /></a> */}
+            <a href='https://www.linkedin.com/in/swapnesh-panda-164b79303/'><Linkedin className='section6text2 mx-2' /></a>
 
           </div>
         </div>
@@ -354,15 +407,15 @@ const Section6 = ({ inView4, myRef4 }) => (
         <div className=''>
           <p className='section6heading'>Contact Us</p>
           <p className='section6text1'>PHONE</p>
-          <p className='section6text2'>+91 9886506163</p>
+          <p className='section6text2'>+91 9348039447</p>
           <p className='section6text1'>EMAIL</p>
-          <p className='section6text2'>gkadkol@starlly.in</p>
+          <p className='section6text2'>business.swapnesh@gmail.com</p>
         </div>
         <div className='pt-5'>
           <p className='section6text1'>SOCIAL</p>
-          <a target="_blank" href="https://www.facebook.com/people/Starlly-Solutions-Pvt-Ltd/100089368485327/?sk=about"><Facebook className='section6text2' /></a>
-          <a target="_blank" href='https://x.com/sayhitospectra'><Twitter className='section6text2 mx-4' /></a>
-          <a target="_blank" href='https://www.linkedin.com/company/starlly-solutions-pvt.-ltd./'><Linkedin className='section6text2 mx-2' /></a>
+          <a href="https://www.instagram.com/swapnesh.io/"><Instagram className='section6text2' /></a>
+          {/* <a  href='https://x.com/sayhitospectra'><Twitter className='section6text2 mx-4' /></a> */}
+          <a href='https://www.linkedin.com/in/swapnesh-panda-164b79303/'><Linkedin className='section6text2 mx-2' /></a>
 
         </div>
       </div>
